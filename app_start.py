ccc011b038e99d0cea5ebced46eec66a9736ae30
progrm-github <30901059+progrm-github@users.noreply.github.com>
2021-10-23 23:02:05 +0900

    Add files via upload

---
+++ app_start.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, redirect
+import pymysql
+from random import *
+from flask import request
+
+db = pymysql.connect(host="db4free.net", user="kuuhaku", passwd="jmjmjm0730", db="kuuhaku", charset="utf8")
+cur = db.cursor()
+
+app = Flask(__name__)
+global int1
+int1 = 0
+@app.route('/')
+def index():
+    sql = "SELECT * from userdata"
+    cur.execute(sql)
+
+    data_list = cur.fetchall()
+
+    
+
+
+    return render_template('main.html', value=data_list)
+@app.route('/write')
+def write():
+    return render_template('write.html')
+@app.route('/loding',methods=['GET', 'POST'])
+def roding():
+    global int1
+    name = request.args.get('name')
+    title = request.args.get('title')
+    result = request.args.get('result')
+    print(result)
+    sql = "SELECT * from userdata"
+    cur.execute(sql)
+
+
+    mycursor = db.cursor()
+    c = randrange(999999)
+
+    sql = "INSERT INTO userdata (`num`, `title`, `writer`, `views`, `context`) VALUES (%s, %s,%s,%s,%s)"
+    val = (str(c), str(title), str(name), '1', str(result))
+
+    mycursor.execute(sql, val)
+
+    db.commit()
+
+    return redirect('/')
+@app.route('/look/<int:articleID>/')
+def board_content(articleID):
+    UserId = articleID
+
+    sql = "SELECT * FROM userdata WHERE num = '{}'".format(UserId)
+
+    cur.execute(sql)
+
+    result = cur.fetchall()
+
+
+
+
+    return render_template("look.html", result=result)
+
+@app.route('/main.html')
+def gomain():
+    return redirect(request.host_url)
+
+@app.route('/delete/<id>')
+def delete(id):
+    sql = "DELETE FROM userdata WHERE num = {}".format(id)
+
+    cur.execute(sql) 
+
+    db.commit() 
+
+    return redirect(request.host_url)
+    
+
+if __name__ == '__main__':
+    app.run()
